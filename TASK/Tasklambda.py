b8081dc390aa580766f9919dc9caaaaf928a9c47
Ravindiran M <ravindiran.m>
2024-07-25 18:26:41 +0530

    task

--- TASK/Tasklambda.py
+++ TASK/Tasklambda.py
@@ -1,7 +1,21 @@
 #sort a dictionary by value in python (lambda function)
-[
-{'name':'shreya','age':15},
-{'name':'pratiksha','age':13}
-{'name':'prerna','age':15}
+# [
+# {'name':'shreya','age':15},
+# {'name':'pratiksha','age':13}
+# {'name':'prerna','age':15}
  
-]
+# ]
+def main():
+    list = [
+        {'name': 'shreya', 'age': 15},
+        {'name': 'pratiksha', 'age': 13},
+        {'name': 'prerna', 'age': 15}
+    ]
+
+    sorted_list = sorted(list, key=lambda val: (val['name'], val['age']))
+
+    for i in sorted_list:
+        print(i)
+
+if __name__ == "__main__":
+    main()
